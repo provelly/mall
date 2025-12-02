78776515169f97f667de83e7340b1d00007a4ed1
provelly <hinseong489@gmail.com>
2025-12-02 17:31:57 +0900

    09. create product details page

---
+++ lib/actions/product.action.ts
@@ -0,0 +1,22 @@
+export default ProductList
+
+'use server'
+
+import { desc } from 'drizzle-orm'
+
+import db from '@/db/drizzle' 
+import { products } from '@/db/schema'
+
+export async function getLatestProducts() {
+  const data = await db.query.products.findMany({
+    orderBy: [desc(products.createdAt)],
+    limit: 4,
+  })
+  return data
+}
+
+export async function getProductBySlug(slug: string) {
+  return await db.query.products.findFirst({
+    where: eq(products.slug, slug),
+  })
+}
